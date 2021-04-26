b9a47cded8c5527d3a30f3e789a5e65580ff81c9
humalotlouder <46416869+humalotlouder@users.noreply.github.com>
2021-04-26 04:48:24 -0500

    Update util.py

--- base/util.py
+++ base/util.py
@@ -1,11 +1,7 @@
 # -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
+
  
 import hashlib, binascii, os
-
-# Inspiration -> https://www.vitoshacademy.com/hashing-passwords-in-python/
 
 def hash_pass( password ):
     """Hash a password for storing."""
