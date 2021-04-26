fb38ef816c237e34c6ff18666f4c5a7f09a9e597
humalotlouder <46416869+humalotlouder@users.noreply.github.com>
2021-04-26 04:41:21 -0500

    Update routes.py

--- home/routes.py
+++ home/routes.py
@@ -1,8 +1,4 @@
 # -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from app.home import blueprint
 from flask import render_template, redirect, url_for, request
 from flask_login import login_required, current_user
